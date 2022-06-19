72573298bf1066254a8ca4ba4ebf9ca0c738add3
minjae <rlaalswo201@gmail.com>
2022-06-19 19:58:38 +0900

    [Update] change data

--- Python/PS/11726.py
+++ Python/PS/11726.py
@@ -4,7 +4,10 @@ res_l = [ 0 for i in range(n + 1)]
 res_l[0] = 0
 res_l[1] = 1
 res_l[2] = 2
-for i in range(3,n+1):
-    res_l[i] = res_l[i-1] + res_l[i-2] # 세로 1개 추가 , 가로 2개 추가
+if n <= 2:
+    print(res_l[n])
+else:
+    for i in range(3,n+1):
+        res_l[i] = res_l[i-1] + res_l[i-2] # 세로 1개 추가 , 가로 2개 추가
 
-print(res_l[n] % 10007)
+    print(res_l[n] % 10007)
