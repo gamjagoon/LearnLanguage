8995259a4a2e67cb30aab752c9b14b84a7075e0c
김민재 <gimminjae@gimminjcBookAir.s2.islab.re.kr>
2022-07-07 15:05:53 +0900

    [Add] ToDo list tutorial end

--- Svelte/todo-app/src/App.svelte
+++ Svelte/todo-app/src/App.svelte
@@ -1,26 +1,32 @@
 <script>
+	import { writable } from 'svelte/store'
 	import Todo from "./Todo.svelte";
 	let title = ''
-	let todo_list = []
+	let todo_list = writable([])
 	let id = 0
 
 	function CreateToDo (todo) {
-		todo_list.push({
+		if (!title.trim()) {
+			return
+		}
+		$todo_list.push({
 			id,
 			title
 		})
-		todo_list = todo_list
+		$todo_list = $todo_list
 		title = ''
 		id += 1
 	}
 </script>
 
 <input type="text"
-	   bind:value={title} />
+	   bind:value={title}
+	   on:keydown={(e) => {e.key === 'Enter' && CreateToDo()}}
+/>
 <button on:click={CreateToDo}>
 	CreateToDo
 </button>
 
-{#each todo_list as todo}
-	<Todo {todo} />
+{#each $todo_list as todo}
+	<Todo {todo_list} {todo}/>
 {/each}
--- Svelte/todo-app/src/Todo.svelte
+++ Svelte/todo-app/src/Todo.svelte
@@ -1,7 +1,51 @@
 <script>
+    export let todo_list
     export let todo // props
+
+    let isEdit = false
+    let title = ""
+
+    function onEdit() {
+        isEdit = true
+        title = todo.title
+    }
+
+    function editOff() {
+        isEdit = false
+    }
+
+    function updateTodo() {
+        isEdit = false 
+        todo.title = title
+    }
+
+    function deleteTodo() {
+        $todo_list = $todo_list.filter(t => t.id !== todo.id)
+    }
 </script>
 
+{#if isEdit}
+    <div>
+        <input bind:value={title}
+               type="text" 
+        on:keydown={(e) => {e.key === 'Enter' && CreateToDo()}}
+        />
+        <button on:click={updateTodo}>
+            OK
+        </button>
+        <button on:click={editOff}>
+            Cancel
+        </button>
+    </div>
+
+{:else}
 <div>
     {todo.title}
+    <button on:click={onEdit}>
+        Edit
+    </button>
+    <button on:click={deleteTodo}>
+        Delete
+    </button>
 </div>
+{/if}
